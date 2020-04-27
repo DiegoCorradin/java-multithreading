f7905828d840d0935f6d23a878ae7b40b8833a86
diego corradin <diego.corradin@marconirovereto.it>
2020-04-27 19:47:42 +0200

    how to get return values from threads

--- Threading/src/app.java
+++ Threading/src/app.java
@@ -1,14 +1,60 @@
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Utente
  */
 public class app {
 
-    
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Future<Integer> future = executor.submit(new Callable<Integer>(){
+            @Override
+            public Integer call() throws Exception {
+                Random random = new Random();
+                int duration = random.nextInt(4000);
+                
+                System.out.println("Starting..");
+                
+                try {
+                    Thread.sleep(duration);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                
+                System.out.println("Finished..");
+                
+                return duration;
+            }
+            
+        });
+        
+        executor.shutdown();
+        
+        
+        try {
+            System.out.println("The result is: " + future.get());
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } catch (ExecutionException ex) {
+            System.out.println(ex);
+        }
+        
+        
+    }
 }
