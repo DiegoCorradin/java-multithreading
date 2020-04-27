3d0f31a9271d92c807a9932e71aa81e0b1006582
diego corradin <diego.corradin@marconirovereto.it>
2020-04-27 19:08:39 +0200

    part 13 start

--- Threading/src/app.java
+++ Threading/src/app.java
@@ -3,29 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author Utente
  */
 public class app {
+
     
-    public static void main(String[] args) throws Exception {
-        Connection.getInstance().connect();
-        
-        ExecutorService executor = Executors.newCachedThreadPool();
-        
-        for(int i = 0; i < 200; i++){
-            executor.submit(new Runnable(){
-                public void run() {
-                    Connection.getInstance().connect();
-                }
-            });
-        }
-        executor.shutdown();
-        executor.awaitTermination(1, TimeUnit.DAYS);
-    }  
 }
