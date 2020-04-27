cd503901fd16011bcfaef43bdf2a4beda0f82579
diego corradin <diego.corradin@marconirovereto.it>
2020-04-27 17:25:18 +0200

    preparazione ambiente

---
+++ Threading/src/Runner.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Utente
+ */
+public class Runner {
+    
+    public void firstThread() throws InterruptedException {
+        
+    }
+    
+    public void secondThread() throws InterruptedException {
+        
+    }
+    
+    public void finished() {
+        
+    }
+}
